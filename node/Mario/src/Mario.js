066978db8adcea49349afb7c59bebc80970929c1
josueeng <josue.programador@hotmail.com>
2025-03-10 17:30:59 -0300

    Commit com os arquivos do projeto mario

--- node/Mario/src/Mario.js
+++ node/Mario/src/Mario.js
@@ -89,53 +89,5 @@ async function getRandomBlock() {
 
 (async function main() {
     console.log(`🏁 A corrida entre ${player1.Nome} e ${player2.Nome} está começando...`);
-    for (let round = 1; round <= 5; round++) {
-        const block = await getRandomBlock();
-        console.log(`🚩 Bloco da pista: ${block}`);
-        const roll1 = await RollDice();
-        const roll2 = await RollDice();
-        console.log(`🎲 ${player1.Nome} rolou ${roll1}`);
-        console.log(`🎲 ${player2.Nome} rolou ${roll2}`);
-        let attribute1, attribute2;
-        switch (block) {
-            case "reta":
-                attribute1 = player1.Velocidade;
-                attribute2 = player2.Velocidade;
-                break;
-            case "curva":
-                attribute1 = player1.Manobrabilidade;
-                attribute2 = player2.Manobrabilidade;
-                break;
-            case "confronto":
-                attribute1 = player1.Poder;
-                attribute2 = player2.Poder;
-                break;
-        }
-        const score1 = roll1 + attribute1;
-        const score2 = roll2 + attribute2;
-        console.log(`🏎️ ${player1.Nome} total: ${score1}`);
-        console.log(`🏎️ ${player2.Nome} total: ${score2}`);
-        if (block === "confronto") {
-            if (score1 > score2) {
-                player2.Pontos = Math.max(0, player2.Pontos - 1);
-                console.log(`💥 ${player2.Nome} perdeu um ponto`);
-            } else if (score2 > score1) {
-                player1.Pontos = Math.max(0, player1.Pontos - 1);
-                console.log(`💥 ${player1.Nome} perdeu um ponto`);
-            } else {
-                console.log(`🤝 Empate!!!`);
-            }
-        } else {
-            if (score1 > score2) {
-                player1.Pontos++;
-                console.log(`🏆 ${player1.Nome} venceu a rodada`);
-            } else if (score2 > score1) {
-                player2.Pontos++;
-                console.log(`🏆 ${player2.Nome} venceu a rodada`);
-            } else {
-                console.log(`🤝 Empate!!!`);
-            }
-        }
-    }
-    console.log(`🏁 Resultado final: ${player1.Nome} ${player1.Pontos} - ${player2.Pontos} ${player2.Nome}`);
+    await Playrace(player1, player2);
 })();
